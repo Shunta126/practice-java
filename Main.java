b182d91310c5c0b897b6c8bb8151a3a83d85fe08
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-12-14 06:59:40 +0000

    create conb

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("ようこそ占いの館へ");
-    System.out.print("あなたの名前を入力してください＞");
-    String name = new java.util.Scanner(System.in).nextLine();
-    System.out.print("あなたの年齢を入力してください＞");
-    int age = new java.util.Scanner(System.in).nextInt();
-    int fortune = new java.util.Random().nextInt(4);
-    fortune++ ;
-    System.out.print(fortune);
-    System.out.println("占いの結果が出ました");
-    System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-    System.out.println("(1:大吉 2:中吉 3:吉 4:凶)");
+    System.out.println("数当てゲーム");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+    System.out.println("0~9の数字を入力してください");
+    int num = new java.util.Scanner(System.in).nextInt();
+    if (ans == num) {
+      System.out.println("当たり！");
+      break;
+    } else {
+      System.out.println("違います！");
+    }
+    }
+    System.out.println("ゲームを終了します。");
   }
 }
