e64bc56ec748bad5045f1a478206f5b63f855e67
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-12-14 06:15:36 +0000

    create name age

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    boolean tenki = true;
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩にいきます");
-    } else {
-      System.out.println("DVDを見ます");
-      System.out.println("寝ます");
-    }
+    System.out.println("ようこそ占いの館へ");
+    System.out.print("あなたの名前を入力してください＞");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.print("あなたの年齢を入力してください＞");
+    int age = new java.util.Scanner(System.in).nextInt();
+    int fortune = new java.util.Random().nextInt(4);
+    fortune++ ;
+    System.out.print(fortune);
+    System.out.println("占いの結果が出ました");
+    System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+    System.out.println("(1:大吉 2:中吉 3:吉 4:凶)");
   }
 }
