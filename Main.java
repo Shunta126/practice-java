de723e744a527351e768840f8bd2b2c5f96e31bc
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-12-18 07:49:19 +0000

    create 9*9

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = new int[5];
-    scores[1] = 30;
-    System.out.println(scores[1]);
+    for (int one = 1; one < 10; one++){
+      for (int two = 1; two < 10; two++){
+        int total = one * two;
+        if (one % 2 == 0){
+          System.out.print(total);
+          System.out.print(" ");
+        } else {
+          System.out.print(total);
+          System.out.print("|");
+        }
+      }
+      System.out.println("");
+    }
   }
 }
